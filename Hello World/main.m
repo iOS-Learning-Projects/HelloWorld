10a65bc0e8e42e1953334961ae15a68ac59ed8ea
Eduardo Vital Alencar Cunha <vitaldu@gmail.com>
2018-02-12 14:06:20 -0200

    just add

--- Hello World/main.m
+++ Hello World/main.m
@@ -7,11 +7,39 @@
 //
 
 #import <Foundation/Foundation.h>
+#define CONST 99999999
 
 int main(int argc, const char * argv[]) {
+    int integer = 20;
+    long longInteger = 20000000002;
+    long long longLongInteger = 3000000000000000003;
+    
+    float number = 5.0001;
+    double precisedNumber = 5.0001;
+
+    BOOL boolean = YES;
+    char character = 'a';
+    char * string = "word";
+
+    const int constante = 1000;
+    
     @autoreleasepool {
-        // insert code here...
         NSLog(@"Hello, World!");
+        
+        NSLog(@"Integer: %d", integer);
+        NSLog(@"Long Integer: %ld", longInteger);
+        NSLog(@"Long Long Integer: %lld", longLongInteger);
+
+        NSLog(@"Float: %f", number);
+        NSLog(@"Double: %lf", precisedNumber);
+
+        NSLog(@"Boolean: %d", boolean);
+
+        NSLog(@"Char: %c", character);
+        NSLog(@"String: %s", string);
+
+        NSLog(@"Const: %d", constante);
+        NSLog(@"Const in Header: %d", CONST);
     }
     return 0;
 }
